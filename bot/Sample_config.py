e46b9b04a6f03b29c77af270e20813f996139ac7
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-03 07:30:03 +0530

    Update Sample_config.py

--- bot/Sample_config.py
+++ bot/Sample_config.py
@@ -6,4 +6,3 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 17795696))
     API_HASH = os.environ.get("API_HASH", "afcbcd249c7d8728d9b213b10d39a649")
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Pytogram_support")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Emlifhbot")
